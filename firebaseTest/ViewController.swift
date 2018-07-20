876d3af4265119312888b3a6c0128e5e78ac4667
hjseo <seohj92@podbbang.com>
2018-07-20 14:44:50 +0900

    프로젝트 생성

---
+++ firebaseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseTest
+//
+//  Created by Admin on 2018. 7. 20..
+//  Copyright © 2018년 hj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
